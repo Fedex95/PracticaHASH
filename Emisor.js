af123880a45455255f477617a53b8f6e5ebec3b5
Fedex95 <fedex95@outlook.es>
2025-05-25 15:02:42 -0500

    Comentarios

--- Emisor.js
+++ Emisor.js
@@ -1,18 +1,23 @@
 const fs = require('fs');
 const crypto = require('crypto');
 
+// Se define el mensaje y clave
 const mensaje = "Read Dead Rdemption 2";
 const clave = "Rockstar";
 
+// Crea un HMAC usando el algoritmo SHA-256, la clave secreta y el mensaje que luego lo convierte a hexadecimal
 const hmac = crypto.createHmac('sha256', clave)
                    .update(mensaje)
                    .digest('hex');
 
+//Agrega el mensaje y el HMAC a un objeto         
 const datos ={
     mensaje: mensaje,
     hmac: hmac
 };
 
+// Guarda el objeto en un archivo JSON
+// Se utiliza JSON.stringify para convertir el objeto a una cadena JSON
 fs.writeFileSync('MesSafe.json', JSON.stringify (datos, null, 4));
 
 console.log("Mensaje y HMAC guardados en MesSafe.json");
--- Receptor.js
+++ Receptor.js
@@ -1,16 +1,24 @@
 const fs = require('fs');
 const crypto = require('crypto');
 
+// Define la clave secreta que debería ser igual a la usada por el emisor
 const clave = "Rockstar";
 
+// Lee el archivo 'MesSafe.json' que contiene el mensaje y su HMAC generado por el emisor
 const datos = JSON.parse(fs.readFileSync('MesSafe.json', 'utf8'));
+
+// Extrae el mensaje y el HMAC del objeto leído
 const mRecibido = datos.mensaje;
 const hmacRecibido = datos.hmac;
 
+// Vuelve a calcular el HMAC localmente con la misma clave y el mensaje recibido
 const hmVerificado = crypto.createHmac('sha256', clave)
     .update(mRecibido)
     .digest('hex');
 
+// Compara el HMAC calculado con el HMAC recibido
+// Si son iguales, el mensaje es auténtico
+// Si son diferentes, el mensaje no es auténtico o la clave no es correcta
 if (hmVerificado === hmacRecibido) {
     console.log("El mensaje es auténtico");
 } else {
